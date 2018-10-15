397a284ef5e4a414f53d2277df891999feadbfe9
spiritofsofya <chanovas@outlook.com>
2018-10-15 20:02:29 +0300

    Реализация функции encrypt для шифра Виженера

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -8,19 +8,18 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    for i, j in enumerate(plaintext):
+        num = ord(j)
+        if 97 <= num <= 122 or 65 <= num <= 90:
+            shift = ord(keyword[i % len(keyword)])
+            shift -= 97 if 97 <= num <= 122 else 65
+            m = num + shift
+            if 97 <= num <= 122 and m > 122:
+                m -= 26
+            elif 65 <= num <= 90 and m > 90:
+                m -= 26
+            ciphertext += chr(m)
+        else:
+            chiphertext += j
     return ciphertext
-
-
-def decrypt_vigenere(ciphertext, keyword):
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    # PUT YOUR CODE HERE
-    return plaintext
