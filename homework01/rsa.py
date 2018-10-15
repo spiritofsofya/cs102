b0d2efac2bf473f93c596ba637cde4c08b92aa84
spiritofsofya <chanovas@outlook.com>
2018-10-16 02:04:48 +0300

    Реализована функция generate_keypair

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -35,7 +35,7 @@ def gcd(a, b):
             a = a - b
         else:
             b = b - a
-    print(a)
+    return a
     pass
 
 
@@ -68,27 +68,14 @@ def generate_keypair(p, q):
         raise ValueError('Both numbers must be prime.')
     elif p == q:
         raise ValueError('p and q cannot be equal')
-
-    n = pq
-    # PUT YOUR CODE HERE
-
-    phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
-
-    # Choose an integer e such that e and phi(n) are coprime
+    n = p * q
+    phi = (p-1) * (q-1)
     e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
-
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 
