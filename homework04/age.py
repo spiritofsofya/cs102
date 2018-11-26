6f7762077ac9633584e3fb3834efc2959044ba34
spiritofsofya <chanovas@outlook.com>
2018-11-26 21:46:05 +0300

    Добавлены функции

--- homework04/age.py
+++ homework04/age.py
@@ -1,7 +1,6 @@
-import datetime as dt
+from datetime import datetime
 from statistics import median
 from typing import Optional
-
 from api import get_friends
 from api_models import User
 
@@ -16,5 +15,24 @@ def age_predict(user_id: int) -> Optional[float]:
     """
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+    friends = get_friends(user_id, "bdate")
 
+    friends = [User(**friend) for friend in friends]
+    ages = []
+
+    for friend in friends:
+        if friend.bdate is not None:
+            data = friend.bdate.split(".")
+            if len(data) == 3:
+                age = datetime.now().year - int(data[2])
+                born_month = int(data[1])
+                born_day = int(data[0])
+                if (datetime.now().month < born_month) or (
+                        datetime.now().month == born_month and datetime.now().day < born_day):
+                    ages.append(age - 1)
+                else:
+                    ages.append(age)
+    if len(ages) > 0:
+        return median(ages)
+    else:
+        return None
