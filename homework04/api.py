f5692d7182daae0f2d67920f221a444763e83e3e
spiritofsofya <chanovas@outlook.com>
2018-11-26 21:38:55 +0300

    Добавлены функции

--- homework04/api.py
+++ homework04/api.py
@@ -1,7 +1,7 @@
 import requests
 import time
-
 import config
+from api_models import Message
 
 
 def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
@@ -13,7 +13,15 @@ def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
     :param max_retries: максимальное число повторных запросов
     :param backoff_factor: коэффициент экспоненциального нарастания задержки
     """
-    # PUT YOUR CODE HERE
+    for i in range(max_retries):
+        try:
+            response = requests.get(url, params=params)
+            return response
+        except requests.RequestException:
+            if i == max_retries - 1:
+                raise
+            delay = backoff_factor * 2 ** i
+            time.sleep(delay)
 
 
 def get_friends(user_id, fields):
@@ -25,7 +33,23 @@ def get_friends(user_id, fields):
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert isinstance(fields, str), "fields must be string"
     assert user_id > 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+
+    query_params = {
+        'domain': config.VK_CONFIG['domain'],
+        'access_token': config.VK_CONFIG['access_token'],
+        'user_id': user_id,
+        'fields': fields,
+        'v': config.VK_CONFIG['version']
+    }
+
+    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v={v}".format(
+        **query_params)
+    response = get(query, query_params)
+    json_doc = response.json()
+    fail = json_doc.get('error')
+    if fail:
+        raise Exception(json_doc['error']['error_msg'])
+    return json_doc['response']['items']
 
 
 def messages_get_history(user_id, offset=0, count=20):
@@ -40,4 +64,38 @@ def messages_get_history(user_id, offset=0, count=20):
     assert isinstance(offset, int), "offset must be positive integer"
     assert offset >= 0, "user_id must be positive integer"
     assert count >= 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+    query_params = {
+        'domain': config.VK_CONFIG['domain'],
+        'access_token': config.VK_CONFIG['access_token'],
+        'user_id': user_id,
+        'offset': offset,
+        'count': count,
+        'v': config.VK_CONFIG['version']
+    }
+
+    messages = []
+    try:
+
+        query = "{domain}/messages.getHistory?access_token={access_token}&user_id={user_id}&v={v}".format(
+            **query_params)
+        response = get(query)
+        json_doc = response.json()
+        fail = json_doc.get('error')
+        if fail:
+            raise Exception(json_doc['error']['error_msg'])
+        count = json_doc['response']['count']
+        while count > 0:
+            query = "{domain}/messages.getHistory?access_token={access_token}/" \
+                    "&user_id={user_id}&offset={offset}&count={count}&v={v}".format(**query_params)
+            response = get(query)
+            json_doc = response.json()
+            fail = json_doc.get('error')
+            if fail:
+                raise Exception(json_doc['error']['error_msg'])
+            messages.extend(json_doc['response']["items"])
+            count -= min(count, 200)
+            query_params['offset'] += 200
+            query_params['count'] = min(count, 200)
+            time.sleep(0.4)
+    finally:
+        return messages
