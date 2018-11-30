7718f58b822a60f83f32b8173614d04280451426
spiritofsofya <chanovas@outlook.com>
2018-11-30 20:20:37 +0300

    -

--- homework04/api.py
+++ homework04/api.py
@@ -4,7 +4,7 @@ import config
 from api_models import Message
 
 
-def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
+def get(url, params={}, timeout=1, max_retries=5, backoff_factor=0.3):
     """ Выполнить GET-запрос
 
     :param url: адрес, на который необходимо выполнить запрос
